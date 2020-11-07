f4853b940bb0853b1810ca1c31c6a764b8349ae8
Matthias Rüedlinger <matthias.ruedlinger@spoud.io>
2020-11-07 13:54:47 +0100

    updated

--- src/test/java/ch/yax/connect/quickstart/transform/UUIDFieldTest.java
+++ src/test/java/ch/yax/connect/quickstart/transform/UUIDFieldTest.java
@@ -9,8 +9,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 
 @ExtendWith(MockitoExtension.class)
 public class UUIDFieldTest {
@@ -22,6 +24,7 @@ public class UUIDFieldTest {
     private static final String FIELD_2_NAME = "bar";
     private static final int FIELD_2_VALUE = 123;
     private static final Schema FIELD_2_SCHEMA = Schema.INT32_SCHEMA;
+    private static final String FIELD_UUID = "uuid";
 
 
     @Test
@@ -29,13 +32,74 @@ public class UUIDFieldTest {
 
         final SourceRecord record = crateSourceRecord(createSchema(), createStruct(), null, null);
 
-        final UUIDField.Value<SourceRecord> value = new UUIDField.Value<>();
-        value.configure(new HashMap<>());
+        final UUIDField.Value<SourceRecord> transform = new UUIDField.Value<>();
+        transform.configure(new HashMap<>());
 
-        final SourceRecord newRecord = value.apply(record);
-        assertThat(newRecord.value().toString()).contains("Struct{foo=value,bar=123,uuid=");
+        final SourceRecord newRecord = transform.apply(record);
+        final Struct newValue = (Struct) newRecord.value();
+
+        assertThat(newValue.get(FIELD_1_NAME)).isEqualTo(FIELD_1_VALUE);
+        assertThat(newValue.get(FIELD_2_NAME)).isEqualTo(FIELD_2_VALUE);
+        assertThat(newValue.get(FIELD_UUID)).isNotNull();
 
     }
+
+    @Test
+    void testKeyWithSchema() {
+
+        final SourceRecord record = crateSourceRecord(null, null, createSchema(), createStruct());
+
+        final UUIDField.Key<SourceRecord> transform = new UUIDField.Key<>();
+        transform.configure(new HashMap<>());
+
+        final SourceRecord newRecord = transform.apply(record);
+        final Struct newKey = (Struct) newRecord.key();
+
+        assertThat(newKey.get(FIELD_1_NAME)).isEqualTo(FIELD_1_VALUE);
+        assertThat(newKey.get(FIELD_2_NAME)).isEqualTo(FIELD_2_VALUE);
+        assertThat(newKey.get(FIELD_UUID)).isNotNull();
+
+    }
+
+    @Test
+    void testValueSchemaless() {
+
+        final Map<String, Object> values = Map.of(FIELD_1_NAME, FIELD_1_VALUE, FIELD_2_NAME, FIELD_2_VALUE);
+
+        final SourceRecord record = crateSourceRecord(null, values, null, null);
+
+        final UUIDField.Value<SourceRecord> transform = new UUIDField.Value<>();
+        transform.configure(new HashMap<>());
+
+        final SourceRecord newRecord = transform.apply(record);
+        final Map<String, Object> newValues = (Map<String, Object>) newRecord.value();
+        assertThat(newValues.size()).isEqualTo(3);
+        assertThat(newValues).contains(entry(FIELD_1_NAME, FIELD_1_VALUE));
+        assertThat(newValues).contains(entry(FIELD_2_NAME, FIELD_2_VALUE));
+
+        assertThat(newValues).containsKey(FIELD_UUID);
+        assertThat(newValues.get(FIELD_UUID)).isNotNull();
+    }
+
+    @Test
+    void testKeySchemaless() {
+
+        final Map<String, Object> key = Map.of(FIELD_1_NAME, FIELD_1_VALUE, FIELD_2_NAME, FIELD_2_VALUE);
+        final SourceRecord record = crateSourceRecord(null, null, null, key);
+
+        final UUIDField.Key<SourceRecord> transform = new UUIDField.Key<>();
+        transform.configure(new HashMap<>());
+
+        final SourceRecord newRecord = transform.apply(record);
+        final Map<String, Object> newKey = (Map<String, Object>) newRecord.key();
+        assertThat(newKey.size()).isEqualTo(3);
+        assertThat(newKey).contains(entry(FIELD_1_NAME, FIELD_1_VALUE));
+        assertThat(newKey).contains(entry(FIELD_2_NAME, FIELD_2_VALUE));
+
+        assertThat(newKey).containsKey(FIELD_UUID);
+        assertThat(newKey.get(FIELD_UUID)).isNotNull();
+    }
+
 
     private Schema createSchema() {
         return SchemaBuilder.struct()
